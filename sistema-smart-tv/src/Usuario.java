05e786cd7494e965a9ad89495293a39b37a942a5
DavydKaio <davyd.kaio@gmail.com>
2023-10-19 23:37:11 -0300

    métodos

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,24 @@
+public class Usuario {
+    public static void main(String[] args) {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual? " + smartTv.canal);
+        System.out.println("Volume atual? " + smartTv.volume);
+
+        System.out.println("===================");
+
+        smartTv.ligar();
+        System.out.println("TV ligada? " + smartTv.ligada);
+        smartTv.desligar();
+        System.out.println("TV ligada? " + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual? " + smartTv.volume);
+
+        smartTv.definirCanal(13);
+        System.out.println("Canal atual? " + smartTv.canal);
+    }
+}
